93332c2e9708aeffbf2bb2a3943a3df2a1ce701b
Cody De Arkland <codydearkland@gmail.com>
2019-01-29 16:26:06 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { RestService } from './rest.service'
+import { Casdata } from './casdata'
+
+export interface Token {
+  cspapitoken: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'cas-demo-app';
+
+  @ViewChild("f") formValues; // Added this
+  cspapitoken: string
+
+  public casdata: Casdata = <Casdata>{};
+
+  constructor(private rs: RestService) {}
+
+  onSubmit(token: Token) {
+    this.rs.newCall(token).subscribe((data: Casdata) => {
+      this.casdata = data;});
+    this.formValues.resetForm();
+  }
+
 }
